02ec7503756623c1d4b86e684dbab397add40a45
xuxb001 <17633975208@163.com>
2022-01-17 14:29:25 +0800

    首页轮播

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import './style/basic.styl'
 
 export default defineComponent({
   name: 'App'
@@ -12,13 +13,13 @@ export default defineComponent({
   </div>
 </template>
 
-<style>
+<style lang="stylus">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="home">
     <el-carousel :interval="4000" type="card" height="400px">
-      <el-carousel-item v-for="item in 6" :key="item">
-        <h3>{{ item }}</h3>
+      <el-carousel-item v-for="item in urlArr" :key="item.code">
+        <img :src="item.code" alt="" />
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -10,17 +10,34 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import url1 from '@/assets/carousel/1.jpg'
+import url2 from '@/assets/carousel/2.jpg'
+import url3 from '@/assets/carousel/3.jpg'
+import url5 from '@/assets/carousel/5.jpg'
+import url6 from '@/assets/carousel/6.jpg'
+import url11 from '@/assets/carousel/11.jpg'
+
+// declare function require(img: string): string // 声明
 
 export default defineComponent({
   name: 'HomeItem',
   setup() {
-    return {}
+    return {
+      urlArr: [
+        { name: '1', code: url1 },
+        { name: '2', code: url2 },
+        { name: '3', code: url3 },
+        { name: '5', code: url5 },
+        { name: '6', code: url6 },
+        { name: '11', code: url11 }
+      ]
+    }
   },
   mounted() {}
 })
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
 .el-carousel__item h3 {
   color: #475669;
   font-size: 14px;
@@ -36,5 +53,8 @@ export default defineComponent({
 
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
+}
+#home :deep(.el-carousel__container) {
+  background: rgba(0, 0, 0, 0.7);
 }
 </style>
