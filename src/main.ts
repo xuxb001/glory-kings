4e252eba637b74bbbfd6f013e4660a6b4060c0d0
xuxb <17633975208@163.com>
2022-06-20 15:54:39 +0800

    queen de layout

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import { key, store } from './store'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/display.css'
 import './assets/styles/index.scss'
 import styleImport from './utils/style-import'
 
---
+++ src/views/queen/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="queen">
+    <el-row :gutter="10">
+        <div class="grid-content bg-purple tips" v-for="item in tips" :key="item">{{item}}</div>
+    </el-row>
+    <br>
+    <el-row :gutter="10">
+      <div class="grid-content bg-purple-light content" v-for="item in 20" :key="item">
+        <div>标题{{item}}</div>
+        <div>Synthetix is a system designed to let users hold and trade synthetic assets whose value can be tied to real-world assets.</div>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      tips:['all apps', 'dif', 'klosd','script','grid','height','false','light','bmdc']
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+  }
+  .tips{
+    padding: 5px 20px;
+    margin: 10px 10px;
+  }
+  .content{
+    margin: 10px 10px;
+    width: 20%;
+  }
+  @media screen and(max-width: 860px) {
+    .tips{
+      width: 25%;
+    }
+    .content{
+      width: 45%;
+    }
+  }
+  @media screen and(max-width: 710px) {
+    .tips{
+      width: 40%;
+    }
+    .content{
+      width: 90%;
+    }
+  }
+  @media screen and(max-width: 590px) {
+    .tips{
+      width: 90%;
+    }
+    .content{
+      width: 90%;
+    }
+  }
+</style>
