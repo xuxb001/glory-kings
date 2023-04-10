011ab9c1b9e345d63d7104009bf256c89f7f2e2e
xuxb <17633975208@163.com>
2023-04-10 15:03:42 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -8,68 +8,68 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    importElementPlus({}),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  base: './', // 类似publicPath，'./'避免打包访问后空白页面，要加上，不然线上也访问不了
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'), // 设置 `@` 指向 `src` 目录
-      find: 'web3',
-      replacement: 'web3/dist/web3.min.js',
-      // "buffer": "buffer",
-      // "stream": "stream-browserify"
+    plugins: [
+        vue(),
+        importElementPlus({}),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+    ],
+    base: './', // 类似publicPath，'./'避免打包访问后空白页面，要加上，不然线上也访问不了
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'), // 设置 `@` 指向 `src` 目录
+            find: 'web3',
+            replacement: 'web3/dist/web3.min.js'
+            // "buffer": "buffer",
+            // "stream": "stream-browserify"
+        }
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets', // 指定静态资源存放路径
+        sourcemap: false, // 是否构建source map 文件
+        terserOptions: {
+            // 生产环境移除console
+            compress: {
+                drop_console: true,
+                drop_debugger: true
+            }
+        }
+    },
+    server: {
+        open: true, // 是否自动在浏览器打开
+        cors: true, // 允许跨域
+        port: 3003, // 端口号
+        // https: false, // 是否开启 https
+        host: '0.0.0.0'
+        // proxy: {
+        //   '/api': 'http://localhost:3003'
+        // }
+    },
+    css: {
+        // 配置 CSS modules 的行为。选项将被传递给 postcss-modules。
+        modules: {},
+        // PostCSS 配置（格式同 postcss.config.js）
+        // postcss-load-config 的插件配置
+        postcss: {},
+        // 指定传递给 CSS 预处理器的选项
+        preprocessorOptions: {
+            scss: {
+                javascriptEnabled: true
+                // additionalData: '@import "@/assets/styles/index.scss";'
+            }
+        }
+    },
+    // 引入第三方的配置
+    optimizeDeps: {
+        include: []
+    },
+    define: {
+        'process.env': process.env
+        // global:{}
     }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets', // 指定静态资源存放路径
-    sourcemap: false, // 是否构建source map 文件
-    terserOptions: {
-      // 生产环境移除console
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
-  },
-  server: {
-    open: true, // 是否自动在浏览器打开
-    cors: true, // 允许跨域
-    port: 3003, // 端口号
-    // https: false, // 是否开启 https
-    host: '0.0.0.0',
-    // proxy: {
-    //   '/api': 'http://localhost:3003'
-    // }
-  },
-  css: {
-    // 配置 CSS modules 的行为。选项将被传递给 postcss-modules。
-    modules: {},
-    // PostCSS 配置（格式同 postcss.config.js）
-    // postcss-load-config 的插件配置
-    postcss: {},
-    // 指定传递给 CSS 预处理器的选项
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true
-        // additionalData: '@import "@/assets/styles/index.scss";'
-      }
-    }
-  },
-  // 引入第三方的配置
-  optimizeDeps: {
-    include: []
-  },
-  define:{
-    'process.env': process.env,
-    // global:{}
-  }
 })
