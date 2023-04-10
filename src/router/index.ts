0c22e7889658d744d3ca10644b3443c7956ebd6e
xuxb <17633975208@163.com>
2023-04-10 14:04:26 +0800

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,69 +1,127 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import(/* webpackChunkName: "about" */'@/views/home/index.vue') // 懒加载组件
-  },
-  {
-    path: '/kingdom',
-    name: 'kingdom',
-    component: () => import(/* webpackChunkName: "about" */'@/views/kingdom/index.vue'),
-    children:[
-      {
-        path: '/king',
-        name: 'king',
-        component: () => import(/* webpackChunkName: "about" */'@/views/kingdom/king/index.vue'),
-      },
-      {
-        path: '/queen',
-        name: 'queen',
-        component: () => import(/* webpackChunkName: "about" */'@/views/kingdom/queen/index.vue'),
-      }]
-  },
-  {
-    path: '/find-earth',
-    name: 'find-earth',
-    component: () => import(/* webpackChunkName: "about" */'@/views/find-earth/index.vue')
-  },
-  {
-    path: '/demo',
-    name: 'demo',
-    component: () => import(/* webpackChunkName: "about" */'@/views/demo/index.vue'),
-    children:[
-      {
-        path: '/demo1',
-        name: 'demo1',
-        component: () => import(/* webpackChunkName: "about" */'@/views/demo/components/demo1/index.vue'),
-      },
-      {
-        path: '/demo2',
-        name: 'demo2',
-        component: () => import(/* webpackChunkName: "about" */'@/views/demo/components/demo2/index.vue'),
-      },
-      {
-        path: '/demo3',
-        name: 'demo3',
-        component: () => import(/* webpackChunkName: "about" */'@/views/demo/components/demo3/index.vue'),
-      },
-      {
-        path: '/demo4',
-        name: 'demo4',
-        component: () => import(/* webpackChunkName: "about" */'@/views/demo/components/demo4/index.vue'),
-      },
-      {
-        path: '/demo5',
-        name: 'demo5',
-        component: () => import(/* webpackChunkName: "about" */'@/views/demo/components/demo5/index.vue'),
-      }
-    ]
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: () =>
+            import(/* webpackChunkName: "about" */ '@/views/home/index.vue') // 懒加载组件
+    },
+    {
+        path: '/kingdom',
+        name: 'kingdom',
+        component: () =>
+            import(/* webpackChunkName: "about" */ '@/views/kingdom/index.vue'),
+        children: [
+            {
+                path: '/king',
+                name: 'king',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/kingdom/king/index.vue'
+                    )
+            },
+            {
+                path: '/queen',
+                name: 'queen',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/kingdom/queen/index.vue'
+                    )
+            }
+        ]
+    },
+    {
+        path: '/find-earth',
+        name: 'find-earth',
+        component: () =>
+            import(/* webpackChunkName: "about" */ '@/views/find-earth/index.vue')
+    },
+    {
+        path: '/demo',
+        name: 'demo',
+        component: () =>
+            import(/* webpackChunkName: "about" */ '@/views/demo/index.vue'),
+        children: [
+            {
+                path: '/demo1',
+                name: 'demo1',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/demo/components/demo1/index.vue'
+                    )
+            },
+            {
+                path: '/demo2',
+                name: 'demo2',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/demo/components/demo2/index.vue'
+                    )
+            },
+            {
+                path: '/demo3',
+                name: 'demo3',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/demo/components/demo3/index.vue'
+                    )
+            },
+            {
+                path: '/demo4',
+                name: 'demo4',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/demo/components/demo4/index.vue'
+                    )
+            },
+            {
+                path: '/demo5',
+                name: 'demo5',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/demo/components/demo5/index.vue'
+                    )
+            }
+        ]
+    },
+    {
+        path: '/anchor-point',
+        name: 'anchor-point',
+        component: () =>
+            import(/* webpackChunkName: "about" */ '@/views/anchor-point/index.vue'),
+        children: [
+            {
+                path: '/anchor-point/one',
+                name: 'one',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/anchor-point/one/index.vue'
+                    )
+            },
+            {
+                path: '/anchor-point/two',
+                name: 'two',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/anchor-point/two/index.vue'
+                    )
+            },
+            {
+                path: '/anchor-point/three',
+                name: 'three',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "about" */ '@/views/anchor-point/three/index.vue'
+                    )
+            }
+        ]
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
