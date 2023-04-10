ccbeef1519351cb77162a1f3d93fc3440e0cbe89
xuxb <17633975208@163.com>
2023-04-10 14:31:27 +0800

    three

---
+++ src/views/anchor-point/three/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="three">threethreethree</div>
+</template>
+
+<script>
+export default {
+    name: 'three',
+    data() {
+        return {}
+    },
+    mounted() {},
+    methods: {}
+}
+</script>
+<style scoped lang="scss"></style>
