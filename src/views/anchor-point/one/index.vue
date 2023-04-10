7b69ce8ae7b735f31454871bd530397f6410dd28
xuxb <17633975208@163.com>
2023-04-10 14:28:25 +0800

    one

---
+++ src/views/anchor-point/one/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="one">oneoneoneoneoneoneoneoneoneoneoneoneoneoneoneoneone</div>
+</template>
+
+<script>
+export default {
+    name: 'one',
+    data() {
+        return {}
+    },
+    mounted() {},
+    methods: {}
+}
+</script>
+<style scoped lang="scss"></style>
