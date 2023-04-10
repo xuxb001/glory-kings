2e9c70888c49b431ca107b9a85dd5f40a293e9ff
xuxb <17633975208@163.com>
2023-04-10 14:54:20 +0800

    change dev name

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,25 +1,25 @@
 <template>
-  <div id="home">
-    <login/>
-    <!-- <router-link to="/demo">
+    <div id="home">
+        <login />
+        <!-- <router-link to="/demo">
       <img :src="xingkong" alt="" />
     </router-link> -->
-  </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
 import url from '@/assets/images/logo.png'
 import xingkong from '@/assets/images/find-earth/xingkong.jpeg'
-import login from "./login.vue"
+import login from './login.vue'
 </script>
 
 <style scoped lang="scss">
-#home{
-  width: 100vw;
-  height: 100vh;
-  img{
-    width: 100%;
-    height: 100%;
-  }
+#home {
+    width: 100vw;
+    height: 100vh;
+    img {
+        width: 100%;
+        height: 100%;
+    }
 }
 </style>
