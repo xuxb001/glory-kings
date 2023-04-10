d2b2d069da1e45233ed657e7a74ba4ba2006f8d6
xuxb <17633975208@163.com>
2023-04-10 15:12:28 +0800

    kingdom

--- src/views/kingdom/index.vue
+++ src/views/kingdom/index.vue
@@ -1,42 +1,44 @@
 <template>
-  <div class="kingdom">
-    <div class="btn-menu">
-      <span v-for="(item, index) in menuList" :key="index">
-        <el-button @click="toRoute(item.path)">{{item.name}}</el-button>
-      </span>
+    <div class="kingdom">
+        <div class="btn-menu">
+            <span v-for="(item, index) in menuList" :key="index">
+                <el-button @click="toRoute(item.path)">{{ item.name }}</el-button>
+            </span>
+        </div>
+        <router-view></router-view>
     </div>
-    <router-view></router-view>
-  </div>
 </template>
 <script lang="ts" setup>
-import { ref, reactive } from "vue"
-import { useRouter } from "vue-router";
-  const router = useRouter();
-  const menuList = reactive([
-      {
-        path:'kingdom/king',
-        name:'king',
-        code:'1'
-      },
-      {
-        path:'kingdom/queen',
-        name:'queen',
-        code:'2'
-      }])
-  const toRoute = (val: string) => {
+import { ref, reactive } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const menuList = reactive([
+    {
+        path: 'kingdom/king',
+        name: 'king',
+        code: '1'
+    },
+    {
+        path: 'kingdom/queen',
+        name: 'queen',
+        code: '2'
+    }
+])
+const toRoute = (val: string) => {
     router.push({ path: val })
-  }
+}
 </script>
 <style lang="scss" scoped>
-.kingdom{
-  .btn-menu{
-    padding: 20px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    span{
-      margin: 0 10px;
-    };
-  }
+.kingdom {
+    .btn-menu {
+        padding: 20px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        span {
+            margin: 0 10px;
+        }
+    }
 }
 </style>
