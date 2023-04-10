a8d0216de85fbc93bd491bb279231c0fbad29a0c
xuxb <17633975208@163.com>
2023-04-10 14:47:01 +0800

    main home

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="home">
-    <el-carousel :interval="4000" type="card" height="400px">
-      <el-carousel-item v-for="item in urlArr" :key="item.code">
-        <img :src="item.code" alt="" />
-      </el-carousel-item>
-    </el-carousel>
-  </div>
+    <div id="home">
+        <el-carousel :interval="4000" type="card" height="400px">
+            <el-carousel-item v-for="item in urlArr" :key="item.code">
+                <img :src="item.code" alt="" />
+            </el-carousel-item>
+        </el-carousel>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -19,15 +19,15 @@ import url6 from '@/assets/images/carousel/6.jpg'
 import url11 from '@/assets/images/carousel/11.jpg'
 // 定义数组对象方法一
 const urlArr: Array<{
-  code: string
-  name: string
+    code: string
+    name: string
 }> = [
-  { name: '1', code: url1 },
-  { name: '2', code: url2 },
-  { name: '3', code: url3 },
-  { name: '5', code: url5 },
-  { name: '6', code: url6 },
-  { name: '11', code: url11 }
+    { name: '1', code: url1 },
+    { name: '2', code: url2 },
+    { name: '3', code: url3 },
+    { name: '5', code: url5 },
+    { name: '6', code: url6 },
+    { name: '11', code: url11 }
 ]
 // 定义数组对象方法二
 // type TestType = {
@@ -57,24 +57,24 @@ const urlArr: Array<{
 
 <style scoped lang="scss">
 .el-carousel__item h3 {
-  color: #475669;
-  font-size: 14px;
-  opacity: 0.75;
-  line-height: 400px;
-  margin: 0;
-  text-align: center;
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 400px;
+    margin: 0;
+    text-align: center;
 }
 
 .el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+    background-color: #99a9bf;
 }
 
 .el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+    background-color: #d3dce6;
 }
 #home {
-  :deep(.el-carousel__container) {
-    background: rgba(0, 0, 0, 0.7);
-  }
+    :deep(.el-carousel__container) {
+        background: rgba(0, 0, 0, 0.7);
+    }
 }
 </style>
